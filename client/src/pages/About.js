bcdbb325ef1d6298e3d23c332d0fb936fb0e7f8d
Chrisplaysharp <cwalden@berkeley.edu>
2020-01-26 20:23:38 -1000

    snail mail

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,22 +10,22 @@ const About = () =>
 
           <div className='tile is-parent'>
             <article className="tile is-child notification is-dark">
-              <p className="title"> <span className='intellectual'> Intellectual </span> </p>
+              <p className="title"> <span className='intellectual'> Consumerist </span> </p>
               <p className="subtitle"></p>
               <p className="content">
-                An aspiring polymath, actively strives to develop capacity across multiple unrealated academic fields and in all areas of human accomplishment: <span className='intellectual'>intellectual</span>, <span className='artistic'>artistic</span>, <span className='social'>socio-spirtual</span>, and <span className='physical'>physical</span>. 
+                 <span className='intellectual'>consumerist</span>, <span className='artistic'>strategist</span>, <span className='social'>operationalist</span>, and <span className='physical'>athlete</span>. 
 
-                Knowleges in: law, logic, math, business, government, justice, physics, philosophy, electronic engineering, computer science, algorithmic puzzles, <span className='artistic'>music</span>, geography, sociology, biology, science, psycology, art, and more.
+                
               </p>
             </article>
           </div>
             
           <div className='tile is-parent'>
             <article className="tile is-child notification is-dark">
-              <p className="title"> <span className='artistic'> Artistic </span> </p>
+              <p className="title"> <span className='artistic'> Strategist </span> </p>
               <p className="subtitle"></p>
               <div className="content">
-                Music (as a creator) I play the Harp, I dabble in ukulele, theory, composition, production, keyboards, DJ, synthesizers, and hip-hop. Music (as a consumer) I listen to a wide range of genres including but not limited to: electronic, video game music, hip-hop, tech-house, reggae, classical, and Indian classical. My favorites acts are Daft Punk, Migos, and Bach. 
+                Strategist
               </div>
             </article>
           </div>
@@ -48,7 +48,7 @@ const About = () =>
                 <p className='titleMain'><strong>Walden, Chris</strong></p>
                 <p className="subtitle"> - Nike Expert on Demand Specialist and life learner.</p>
                 <div className="content">
-                  Likes javascript, mac, and git. More about me is distributed through out this site, welcome.
+                  North America Consumer Services | Nike Direct Digital Commerece
                 </div>
               </div>
             </article>
@@ -62,17 +62,17 @@ const About = () =>
 
           <div className='tile is-parent'>
             <article className='tile is-child notification is-dark'>
-              <p className="title"> <span className='social'> Socio-Spirtual </span> </p>
+              <p className="title"> <span className='social'> Operationalist </span> </p>
               <p className="subtitle"> </p>
               <div className='content'>
-                Loves to hear about about others intrests. Spontaneous, and cherishes the precense and company of all whom come to pass. Does good. Life mission to leave this place better than it was found. Social interactions between humans can be described as a network. Not all is known about counciousness, mind mysteries are inescapable. Science bears many truths; we all will turn to worm food. Life and Love are precious.
+                Operationalist
               </div>
             </article>
           </div>
 
           <div className='tile is-parent'>
             <article className="tile is-child notification is-dark">
-              <p className="title"> <span className='physical'> Physical </span> </p>
+              <p className="title"> <span className='physical'> Athlete </span> </p>
               <p className="subtitle"> </p>
               <div className='content'>
                 Exercises to hack brain in order to increase learning. A high performance track athlete for 10+ years. High School State Champion. NCAA All-American. All-Pac12. 4:01 mile. 13:59 5k. 29:40 10k. Backpacking, cycling, and lifting are most enjoied modes.
