7e2b5f4db1857cef73961976652445e5c47f3d9f
Chrisplaysharp <cwalden@berkeley.edu>
2021-03-06 08:41:01 -1000

    brooks2

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -13,7 +13,7 @@ const About = () =>
               <p className="title"> <span className='intellectual'> Athlete </span> </p>
               <p className="subtitle"></p>
               <p className="content">
-                We are Nike athletes this is what we believe. The best way to know how to serve an athlete is by being one, making sport a habit! As a high performance track athlete for 10+ years, Nike helped me to a lot: a High School State Championship, NCAA All-American, 4:01 mile, 13:59 5k, and a 66:55 Half-Marathon. Even visted WHQ for Nike Cross Country Nationals and met many life long companions, together in sport. This great life expereince has set me up well as a 
+                We are athletes this is what we believe. The best way to know how to serve an athlete is by being one, making sport a habit! As a high performance track athlete for 10+ years, Nike helped me to a lot: a High School State Championship, NCAA All-American, 4:01 mile, 13:59 5k, and a 66:55 Half-Marathon. Even was invited to the very first <strong>Brooks PR Invite </strong> in Seattle and met many life long companions, together in sport. This great life expereince has set me up well as a 
                  <span className='physical'> consumerist</span>, <span className='artistic'>operationalist</span>, and <span className='social'>strategist</span>.
 
                 
@@ -53,13 +53,12 @@ const About = () =>
             <article className="tile is-child notification walden centroid">
               <div className="">
                 <p className='titleMain'><strong>Chris Walden</strong></p>
-                <p className="subtitle"> - Nike Expert on Demand Specialist</p>
+                <p className="subtitle"> - Guru Field Rep - Portland</p>
                 <div className="content">
-                  - North America Consumer Services <br /> 
-                  - Nike Direct Digital Commerece <br />
+                  - Brooks Running <br /> 
 
                   <p>
-                    Flexes advanced computer literacy for professional excel, word, web documents. Avid learner with high energy and a great attitude. Desires to solve problems and improve upon existing methods. Approaches challenges from different angles, and yearns for collaboration with others to facilitate effective communication and service for Nike members. 
+                    Flexes advanced computer literacy for professional excel, word, web documents. Avid learner with high energy and a great attitude. Desires to solve problems and improve upon existing methods. Approaches challenges from different angles, and yearns for collaboration with others to facilitate effective communication and service for runners. 
                   </p>
 
                 </div>
@@ -78,7 +77,7 @@ const About = () =>
               <p className="title"> <span className='physical'> Consumerist </span> </p>
               <p className="subtitle"> </p>
               <div className='content'>
-                <p>As a true consumerists obessesed with serving the Nike member with the same exemplarly treatment we give our Elite athletes, every interaction needs to be approached as if the member is meeting Nike for the first time.</p>
+                <p>As a true consumerists obessesed with serving all runners with the same exemplarly treatment we give our Elite athletes, every interaction needs to be approached as if the member is meeting Brooks for the first time.</p>
                 <p> With a deep connection to the brand getting served in the same way in my first big sports moments, I am uniquely equipped to pay it forward.</p>
 
               </div>
@@ -87,10 +86,10 @@ const About = () =>
 
           <div className='tile is-parent'>
             <article className="tile is-child notification is-dark">
-              <p className="title"> <span className='social'> Strategist</span> </p>
+              <p className="title"> <span className='social'> Strategist </span> </p>
               <p className="subtitle"> </p>
               <div className='content'>
-                 Strategizing as an Expert, comes in a cascade. Starting with leaderships yearly 3 year plan, on to quaterly, to monthly, to each week, day and member interaction. Staying tuned to seasonal product lines as they cool off and warm up, is essential in keeping Plus members engaged and re-engaging. 
+                 Strategizing as an Expert, comes in a cascade. Starting with leaderships yearly 3 year plan, on to quaterly, to monthly, to each week, day and member interaction. Staying tuned to seasonal product lines as they cool off and warm up, is essential in keeping runners engaged and re-engaging. 
               </div>
             </article>
           </div> 
